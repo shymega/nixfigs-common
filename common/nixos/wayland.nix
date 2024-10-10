5a4c72e81ff1ca928d7e608bda3dc9a89fad77f8
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-10 22:55:25 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ common/nixos/wayland.nix
@@ -0,0 +1,74 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ pkgs, ... }:
+let
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway DISPLAY PATH XDG_SESSION_TYPE
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+      '';
+  };
+in
+{
+  programs.sway = {
+    enable = true;
+    package = pkgs.sway;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      alacritty
+      clipman
+      configure-gtk
+      dbus-sway-environment
+      grim
+      kanshi
+      mako
+      slurp
+      slurp
+      sway-contrib.grimshot
+      swayidle
+      swaylock
+      waybar
+      waybar
+      wayland
+      wdisplays
+      wf-recorder
+      wl-clipboard
+      wofi
+      xdg-utils
+    ];
+    extraSessionCommands = ''
+      export MOZ_ENABLE_WAYLAND=1
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export SDL_VIDEODRIVER=wayland
+      export SSH_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export SUDO_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      export XDG_CURRENT_DESKTOP=sway
+      export XDG_SESSION_TYPE=wayland
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
+  };
+}
---
+++ common/nixos/xdg.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+_:
+{
+  xdg.portal = {
+    enable = true;
+    config.common.default = "wlr;gtk";
+    wlr.enable = true;
+    xdgOpenUsePortal = true;
+  };
+}
