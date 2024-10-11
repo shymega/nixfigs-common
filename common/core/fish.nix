de12abf59eeeee760f6a2e8cc08114fea4a0e064
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-11 21:52:03 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ common/core/fish.nix
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ lib, config, ... }:
+let
+  inherit (lib) mkOption mkIf;
+  inherit (lib.types) bool;
+in
+{
+  options = {
+    nixfigs.shells.fish = {
+      enable = mkOption {
+        type = bool;
+        description = "Enables the Fish shell.";
+        default = false;
+      };
+      allConf = mkOption {
+        type = bool;
+        description = "Enables all configuration for the Fish shell.";
+        default = config.nixfigs.shells.fish.enable;
+      };
+    };
+  };
+
+  config = mkIf config.nixfigs.shells.fish.enable {
+    programs.fish = {
+      enable = true;
+      vendor = {
+        completions.enable = true;
+        config.enable = true;
+        functions.enable = true;
+      };
+    };
+  };
+}
