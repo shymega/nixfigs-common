dcee9f4efc64c673f0883715c3c1993cf4682083
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-11 21:52:03 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ common/nixos/xdg.nix
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ pkgs, ... }:
+{
+  xdg.portal = {
+    enable = true;
+    config.common.default = "wlr;gtk";
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-wlr ];
+    wlr.enable = true;
+    xdgOpenUsePortal = true;
+  };
+}
