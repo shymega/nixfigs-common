af5db165b9b9168dd3c766f5e0b2e267d1449d3b
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-11 21:52:03 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ common/nixos/bluetooth.nix
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
+}
