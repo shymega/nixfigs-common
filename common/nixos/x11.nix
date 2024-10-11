cfe8e86a660de101d7df039184d13f2b8c227621
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-11 21:52:03 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ common/nixos/x11.nix
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ config, lib, pkgs, ... }:
+let
+  swayConfig = pkgs.writeText "greetd-sway-config" ''
+        # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+        exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP; \
+    	${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+        bindsym Mod4+shift+e exec swaynag \
+          -t warning \
+          -m 'What do you want to do?' \
+          -b 'Poweroff' 'systemctl poweroff' \
+          -b 'Reboot' 'systemctl reboot'
+  '';
+in
+{
+  services = {
+    displayManager.defaultSession = "sway";
+    xserver = {
+      enable = true;
+      displayManager = {
+        startx.enable = true;
+        gdm = {
+          enable = false;
+          autoSuspend = false;
+        };
+      };
+
+      desktopManager = {
+        plasma5.enable = true;
+        cinnamon.enable = false;
+        gnome.enable = true;
+      };
+      xkb.layout = "us";
+    };
+    libinput.enable = true;
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+        };
+      };
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    ${pkgs.zsh}/bin/zsh --login -c "${pkgs.sway}"
+    ${pkgs.zsh}/bin/zsh --login -c "dwl"
+    ${pkgs.zsh}/bin/zsh --login -c "startplasma-x11"
+    ${pkgs.zsh}/bin/zsh --login -c "startplasma-wayland"
+    ${pkgs.zsh}/bin/zsh --login
+  '';
+
+  environment.sessionVariables.NIX_GSETTINGS_OVERRIDES_DIR =
+    let
+      cfg = config.services.xserver.desktopManager.gnome;
+      nixos-background-light = pkgs.nixos-artwork.wallpapers.simple-blue;
+      nixos-background-dark = pkgs.nixos-artwork.wallpapers.simple-dark-gray;
+      flashbackEnabled = cfg.flashback.enableMetacity || lib.length cfg.flashback.customSessions > 0;
+      nixos-gsettings-desktop-schemas = pkgs.gnome.nixos-gsettings-overrides.override {
+        inherit (cfg) extraGSettingsOverrides extraGSettingsOverridePackages favoriteAppsOverride;
+        inherit flashbackEnabled nixos-background-dark nixos-background-light;
+      };
+    in
+    lib.mkForce (pkgs.glib.getSchemaPath nixos-gsettings-desktop-schemas);
+
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.gnome.seahorse.out}/libexec/seahorse/ssh-askpass";
+}
