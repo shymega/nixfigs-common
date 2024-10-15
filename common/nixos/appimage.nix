4386e67a65c28481043a5340dc44987e2edf5e5d
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-16 00:25:36 +0100

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/appimage.nix
+++ common/nixos/appimage.nix
@@ -2,13 +2,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-{ pkgs, ... }: {
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.lib.getExe pkgs.appimage-run}";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+{
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
   };
 }
