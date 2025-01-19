8e68fab8fd22f56758cd8b1f56bac6b37044fda6
Dom Rodriguez <shymega@shymega.org.uk>
2025-01-19 20:10:24 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/steam-hardware.nix
+++ common/nixos/steam-hardware.nix
@@ -11,7 +11,7 @@
 with lib; let
   inherit (libx.roleUtils) checkRoles;
   inherit (config.nixfigs.meta) rolesEnabled;
-  enabled = roleUtils.checkRoles ["gaming" "steam-deck" "jovian"] config.nixfigs.meta.rolesEnabled;
+  enabled = checkRoles ["gaming" "steam-deck" "jovian"] config.nixfigs.meta.rolesEnabled;
 in {
   config = mkIf enabled {
     hardware.steam-hardware.enable = true;
