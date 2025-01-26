deaa1d6cf87ccd087a3d59be724c89529ad6694d
Dom Rodriguez <shymega@shymega.org.uk>
2025-01-26 16:04:38 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/custom-systemd-units/power-mangement.nix
+++ common/nixos/custom-systemd-units/power-mangement.nix
@@ -9,11 +9,11 @@
   ...
 }: let
   inherit (config.networking) hostName;
-  inherit (lib) getExe' hasInfix;
+  inherit (lib) getExe';
 in {
   systemd = {
     services = {
-      desktop-power-maximum-tdp = lib.mkIf (hostName == "NEO-LINUX" || hostName == "MORPHEUS-LINUX" || hasInfix hostName "DEUSEX") {
+      desktop-power-maximum-tdp = lib.mkIf (hostName == "NEO-LINUX" || hostName == "MORPHEUS-LINUX" || hostName == "DEUSEX-LINUX") {
         description = "Change TDP to maximum TDP when on AC power";
         wantedBy = [
           "multi-user.target"
@@ -30,7 +30,7 @@ in {
         '';
       };
 
-      portable-power-saving-tdp = lib.mkIf (hostName == "MORPHEUS-LINUX" || hasInfix hostName "DEUSEX") {
+      portable-power-saving-tdp = lib.mkIf (hostName == "MORPHEUS-LINUX" || hostName == "DEUSEX-LINUX") {
         description = "Change TDP to power saving TDP when on battery power";
         wantedBy = ["battery.target"];
         unitConfig = {
@@ -43,7 +43,7 @@ in {
         '';
       };
 
-      powertop = lib.mkIf (hostName == "MORPHEUS-LINUX" || hostName == "TWINS-LINUX" || hasInfix hostName "DEUSEX") {
+      powertop = lib.mkIf (hostName == "MORPHEUS-LINUX" || hostName == "TWINS-LINUX" || hostName == "DEUSEX-LINUX") {
         description = "Auto-tune Power Management with powertop";
         unitConfig = {
           RefuseManualStart = true;
