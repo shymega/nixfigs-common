2ccd97f1a492e743ddab9558921abeaba9a6d5b4
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:00 +0000

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/backups.nix
+++ common/nixos/backups.nix
@@ -13,9 +13,7 @@
       repositoryFile = config.age.secrets."restic_repo".path;
       passwordFile = config.age.secrets."restic_pw".path;
 
-      paths = [
-        "${config.users.users.dzrodriguez.home}"
-      ];
+      paths = [ "${config.users.users.dzrodriguez.home}" ];
 
       pruneOpts = [
         "--keep-daily 7"
