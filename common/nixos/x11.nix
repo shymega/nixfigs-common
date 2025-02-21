7c1daa177a3222c81bb2b368833c8981c8850031
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:01 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/x11.nix
+++ common/nixos/x11.nix
@@ -5,55 +5,69 @@
 {
   pkgs,
   lib,
+  libx,
+  hostRoles,
+  config,
   ...
-}: let
-  kanshiConfig = pkgs.writeText "kanshi-config" ''${builtins.readFile ./config/kanshi/config}'';
+}:
+with lib; let
   swayConfig = pkgs.writeText "greetd-sway-config" ''
     # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-    exec "${pkgs.lib.getExe pkgs.kanshi} -c ${kanshiConfig}"
-    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP; ${pkgs.lib.getExe pkgs.greetd.gtkgreet} -l; swaymsg exit"
+    exec "${getExe pkgs.kanshi} -c /etc/greetd/kanshi-config"
+    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP; ${getExe pkgs.greetd.gtkgreet} -l; swaymsg exit"
     bindsym Mod4+shift+e exec swaynag \
       -t warning \
       -m 'What do you want to do?' \
       -b 'Poweroff' 'systemctl poweroff' \
       -b 'Reboot' 'systemctl reboot'
   '';
+  enabled = libx.roleUtils.checkRoles ["workstation" "work"] hostRoles;
 in {
-  services = {
-    displayManager.defaultSession = "sway";
-    xserver = {
-      enable = true;
-      displayManager = {
-        startx.enable = true;
-        gdm = {
-          enable = false;
-          autoSuspend = false;
-        };
-      };
-      desktopManager.gnome.enable = true;
-      xkb.layout = "us";
-    };
+  config = mkIf enabled {
+    environment.etc."greetd/kanshi-config".source = "${config.users.users."dzrodriguez".home}/.config/kanshi/config";
 
-    desktopManager = {
-      plasma6.enable = true;
-    };
-    libinput.enable = true;
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.lib.getExe pkgs.sway} --config ${swayConfig}";
+    services = {
+      displayManager.defaultSession = "sway";
+      xserver = {
+        enable = true;
+        displayManager = {
+          startx.enable = true;
+          gdm = {
+            enable = false;
+            autoSuspend = false;
+          };
         };
-        inital_session.user = "dzrodriguez";
+        desktopManager.gnome.enable = true;
+        xkb.layout = "us";
+      };
+
+      desktopManager = {
+        plasma6.enable = true;
+      };
+      libinput.enable = true;
+      greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${getExe pkgs.sway} --config ${swayConfig}";
+          };
+          inital_session.user = "dzrodriguez";
+        };
       };
     };
+    environment.etc."greetd/environments".text = let
+      sway-wrapped-hw = pkgs.writeShellScript "sway-wrapped-hw" ''
+        #!/bin/sh
+        export WLR_NO_HARDWARE_CURSORS=1
+        exec ${getExe pkgs.sway} --unsupported-gpu
+      '';
+    in ''
+      ${sway-wrapped-hw}
+      ${getExe pkgs.dwl}
+      startplasma-wayland
+      startplasma-x11
+      zsh
+    '';
+    programs.ssh.askPassword = mkForce "${getExe pkgs.ksshaskpass}/bin/ksshaskpass";
   };
-  environment.etc."greetd/environments".text = ''
-    ${pkgs.lib.getExe pkgs.sway}
-    ${pkgs.lib.getExe pkgs.dwl}
-    plasmax11
-    plasma
-    zsh
-  '';
-  programs.ssh.askPassword = lib.mkForce "${pkgs.ksshaskpass}/bin/ksshaskpass";
 }
