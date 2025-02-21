898671de2157937d0a945ec770138ab85b2c5175
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:00 +0000

    feat(sway): Add support for Kanshi
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/utils/sync.nix
+++ common/nixos/utils/sync.nix
@@ -9,10 +9,10 @@
       #! ${pkgs.stdenv.shell}
       set -eu
 
-      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*sync-conflict*" -print -delete
-      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".#*" -print -delete
-      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname "*~*" -print -delete
-      ${pkgs.coreutils.outPath}/bin/find /home/dzr/{Documents,Multimedia,projects} -type f -iname ".syncthing*" -print -delete
+      ${pkgs.lib.getExe' pkgs.coreutils "find"} /home/dzr/{Documents,Multimedia,projects} -type f -iname "*sync-conflict*" -print -delete
+      ${pkgs.lib.getExe' pkgs.coreutils "find"} /home/dzr/{Documents,Multimedia,projects} -type f -iname ".#*" -print -delete
+      ${pkgs.lib.getExe' pkgs.coreutils "find"} /home/dzr/{Documents,Multimedia,projects} -type f -iname "*~*" -print -delete
+      ${pkgs.lib.getExe' pkgs.coreutils "find"} /home/dzr/{Documents,Multimedia,projects} -type f -iname ".syncthing*" -print -delete
     '')
   ];
 }
--- common/nixos/x11.nix
+++ common/nixos/x11.nix
@@ -6,10 +6,65 @@
 
 { config, lib, pkgs, ... }:
 let
+  kanshiConfig = pkgs.writeText "config.kanshi" ''
+    profile desk_home_mini_pc {
+        output "LG Electronics LG Ultra HD 0x0009B7B4" enable mode 3840x2160 position 0,0 scale 1.55
+        output "BNQ BENQ E2220HD SBA04454019" enable mode 1920x1080 position 2194,0
+    }
+
+    profile default_gpdwm2 {
+      output "Japan Display Inc. GPD1001H 0x00000001" enable mode 2560x1600 scale 1.50 position 0,0
+    }
+
+    profile home_office_gpdwm2 {
+      output "Japan Display Inc. GPD1001H 0x00000001" disable
+      output "LG Electronics LG Ultra HD 0x0009B7B4" enable mode 3840x2160 position 0,0 scale 1.55
+      output "BNQ BENQ E2220HD SBA04454019" enable mode 1920x1080 position 2482,0 scale 1.00
+    }
+
+    profile default_thinkpad_x270 {
+        output "Unknown 0x226D 0x00000000" enable mode 1920x1080 position 0,0
+    }
+
+    profile desk_home_thinkpad_x270 {
+        output "Unknown 0x226D 0x00000000" disable	
+        output "LG Electronics LG Ultra HD 0x0000BFB4" enable mode 3840x2160 position 0,0 scale 1.55
+        output "BNQ BENQ E2220HD SBA04454019" enable mode 1920x1080 position 2194,0
+    }
+
+    profile default_codethink {
+      output "California Institute of Technology 0x1404 Unknown" enable mode 1920x1200 scale 1.00 position 0,0
+    }
+
+    profile home_office_codethink {
+      output "California Institute of Technology 0x1404 Unknown" disable
+      output "LG Electronics LG Ultra HD 0x0009B7B4" enable mode 3840x2160 position 0,0 scale 1.55
+      output "BNQ BENQ E2220HD SBA04454019" enable mode 1920x1080 position 2476,0 scale 1.00
+    }
+
+    profile mirror_codethink_hdmi {
+        output "California Institute of Technology 0x1404 Unknown" enable mode 1920x1200 scale 1.00 position 0,0
+        output HDMI-A-1 enable mode 1920x1080 position 1920,0
+        exec wl-present mirror eDP-1 --fullscreen-output HDMI-A-1 --fullscreen
+    }
+
+    profile mirror_gpd_wm2_hdmi {
+      output "Japan Display Inc. GPD1001H 0x00000001" enable mode 2560x1600 scale 1.50 position 0,0
+        output HDMI-A-1 enable mode 1920x1080 position 1920,0
+        exec wl-present mirror eDP-1 --fullscreen-output HDMI-A-1 --fullscreen
+    }
+
+    profile mirror_thinkpad_x270_hdmi {
+        output "Unknown 0x226D 0x00000000" enable mode 1920x1080 position 0,0
+        output HDMI-A-1 enable mode 1920x1080 position 1920,0
+        exec wl-present mirror eDP-1 --fullscreen-output HDMI-A-1 --fullscreen
+    }  
+  '';
   swayConfig = pkgs.writeText "greetd-sway-config" ''
         # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+        exec "${pkgs.lib.getExe pkgs.kanshi} -c ${kanshiConfig}"
         exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP; \
-    	${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+    	${pkgs.lib.getExe pkgs.greetd.gtkgreet} -l; swaymsg exit"
         bindsym Mod4+shift+e exec swaynag \
           -t warning \
           -m 'What do you want to do?' \
@@ -32,8 +87,8 @@ in
 
       desktopManager = {
         plasma5.enable = true;
-        cinnamon.enable = false;
-        gnome.enable = true;
+        cinnamon.enable = true;
+        gnome.enable = false;
       };
       xkb.layout = "us";
     };
@@ -42,18 +97,18 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+          command = "${pkgs.lib.getExe' pkgs.sway "sway"} --config ${swayConfig}";
         };
       };
     };
   };
 
   environment.etc."greetd/environments".text = ''
-    ${pkgs.zsh}/bin/zsh --login -c "${pkgs.sway}"
-    ${pkgs.zsh}/bin/zsh --login -c "dwl"
-    ${pkgs.zsh}/bin/zsh --login -c "startplasma-x11"
-    ${pkgs.zsh}/bin/zsh --login -c "startplasma-wayland"
-    ${pkgs.zsh}/bin/zsh --login
+    ${pkgs.lib.getExe pkgs.zsh} --login -c "sway"
+    ${pkgs.lib.getExe pkgs.zsh} --login -c "dwl"
+    ${pkgs.lib.getExe pkgs.zsh} --login -c "startplasma-x11"
+    ${pkgs.lib.getExe pkgs.zsh} --login -c "startplasma-wayland"
+    ${pkgs.lib.getExe pkgs.zsh} --login
   '';
 
   environment.sessionVariables.NIX_GSETTINGS_OVERRIDES_DIR =
@@ -69,5 +124,5 @@ in
     in
     lib.mkForce (pkgs.glib.getSchemaPath nixos-gsettings-desktop-schemas);
 
-  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.gnome.seahorse.out}/libexec/seahorse/ssh-askpass";
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.lib.getExe' pkgs.gnome.seahorse "ssh-askpass"}";
 }
