aac8abf42d8bc9e04e771114dc6ecfa49634742b
Dom Rodriguez <shymega@shymega.org.uk>
2026-04-05 19:26:33 +0100

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/nixos/inst_packages.nix
+++ common/nixos/inst_packages.nix
@@ -3,30 +3,34 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs.unstable; [
-    acpi
-    aria2
-    curl
-    encfs
-    fido2luks
-    fuse
-    git
-    gnupg
-    goimapnotify
-    htop
-    ifuse
-    iw
-    libimobiledevice
-    lm_sensors
-    nano
-    nvme-cli
-    pciutils
-    powertop
-    smartmontools
-    solo2-cli
-    syncthing
-    tmux
-    usbutils
-    wget
-  ];
+  environment.systemPackages = with pkgs.unstable;
+    [
+      acpi
+      aria2
+      curl
+      encfs
+      fido2luks
+      fuse
+      git
+      gnupg
+      goimapnotify
+      htop
+      ifuse
+      iw
+      libimobiledevice
+      lm_sensors
+      nano
+      nvme-cli
+      pciutils
+      powertop
+      smartmontools
+      solo2-cli
+      syncthing
+      tmux
+      usbutils
+      wget
+    ]
+    ++ (with pkgs; [
+      hyprscope
+    ]);
 }
