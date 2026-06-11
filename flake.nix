c275ffba159572fc575187f40bc2a865164d9111
Dom Rodriguez <shymega@shymega.org.uk>
2026-06-11 19:35:34 +0100

    chore: Bump to Hyprland v0.55.4
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -106,7 +106,7 @@
     impermanence.url = "github:nix-community/impermanence";
     nm2nix.url = "github:Janik-Haag/nm2nix";
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.55.3";
+      url = "github:hyprwm/Hyprland?ref=v0.55.4";
     };
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins?ref=v0.55.0";
